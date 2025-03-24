1913b99f066ecfb361bf21662b6e9702ca4c0bad
Michael Cornall <mik.reds.forever@gmail.com>
2025-03-24 19:47:40 +0000

    Create company app

---
+++ Torn/company/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('rackets/', views.rackets_list, name='rackets_list'),
+    path('', views.rackets_list, name='rackets_list')
+]
