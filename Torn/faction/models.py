7a1e97f1ce6e6cebf74911775eb13d4e07e51876
Michael Cornall <mik.reds.forever@gmail.com>
2025-03-25 18:18:50 +0000

    Create faction model

--- Torn/faction/models.py
+++ Torn/faction/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Faction(models.Model):
+    faction_id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    tag = models.CharField(max_length=5)
+    respect = models.IntegerField()
+
+    def __str__(self):
+        return self.name
