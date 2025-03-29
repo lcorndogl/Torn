b57490793963ce453f5dd12168aa546d93fa8a0b
Michael Cornall <mik.reds.forever@gmail.com>
2025-03-29 15:01:10 +0000

    Script changes, admin panel amendments

--- Torn/faction/management/commands/create_faction_data.py
+++ Torn/faction/management/commands/create_faction_data.py
@@ -14,45 +14,63 @@ class Command(BaseCommand):
     help = 'Fetch faction data from the Torn API and insert factions ranked Platinum II or higher into the database'
 
     def handle(self, *args, **kwargs):
-        url = 'https://api.torn.com/v2/torn/factionhof?limit=100&cat=rank'
+        base_url = 'https://api.torn.com/v2/torn/factionhof'
         headers = {
             'accept': 'application/json',
             'Authorization': f'ApiKey {API_KEY}'
         }
-        self.stdout.write(self.style.NOTICE(f'Fetching data from {url}'))
-        response = requests.get(url, headers=headers)
+        offset = 0
+        limit = 100
+        all_factions = []
 
-        if response.status_code != 200:
-            self.stdout.write(self.style.ERROR(
-                f'Failed to fetch data. Status code: {response.status_code}'))
-            return
+        while True:
+            url = f'{base_url}?limit={limit}&offset={offset}&cat=rank'
+            self.stdout.write(self.style.NOTICE(f'Fetching data from {url}'))
+            response = requests.get(url, headers=headers)
 
-        data = response.json()
-        self.stdout.write(self.style.NOTICE(f'Response data: {data}'))
+            if response.status_code != 200:
+                self.stdout.write(self.style.ERROR(
+                    f'Failed to fetch data. Status code: {response.status_code}'))
+                break
 
-        # Filter factions ranked Platinum II or higher
-        valid_ranks = ['Platinum II', 'Platinum I',
-                       'Diamond III', 'Diamond II', 'Diamond I']
-        factions = [
-            faction for faction in data.get('factionhof', [])
-            if faction.get('rank') in valid_ranks
-        ]
+            data = response.json()
+            factions = [
+                faction for faction in data.get('factionhof', [])
+                if faction.get('rank') in ['Platinum II', 'Platinum III'] or 'Diamond' in faction.get('rank', '')
+            ]
 
-        if not factions:
+            if not factions:
+                self.stdout.write(self.style.NOTICE('No more factions found.'))
+                break
+
+            all_factions.extend(factions)
+            offset += limit
+
+        if not all_factions:
             self.stdout.write(self.style.WARNING(
                 'No factions found with rank Platinum II or higher.'))
             return
 
-        # Insert or update factions in the database
-        for faction_data in factions:
-            faction, created = FactionList.objects.update_or_create(
+        # Get existing faction IDs from the database
+        existing_faction_ids = set(
+            FactionList.objects.values_list('faction_id', flat=True))
+
+        # Filter out factions that are already in the database
+        new_factions = [
+            FactionList(
                 faction_id=faction_data['id'],
-                defaults={
-                    'name': faction_data['name'],
-                    # Use .get() to handle missing 'tag'
-                    'tag': faction_data.get('tag', '')
-                }
+                name=faction_data['name'],
+                tag=faction_data.get('tag', '')
             )
+            for faction_data in all_factions
+            if faction_data['id'] not in existing_faction_ids
+        ]
+
+        # Bulk insert new factions
+        if new_factions:
+            FactionList.objects.bulk_create(new_factions)
             self.stdout.write(self.style.SUCCESS(
-                f'Successfully {"created" if created else "updated"} faction {faction.name} with rank {faction_data["rank"]}'
+                f'Successfully added {len(new_factions)} new factions to the database.'
             ))
+        else:
+            self.stdout.write(self.style.NOTICE('No new factions to add.'))
--- Torn/faction/models.py
+++ Torn/faction/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
+
 class FactionList(models.Model):
     faction_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=255)
     tag = models.CharField(max_length=5)
 
     def __str__(self):
-        return self.name
+        return str(self.faction_id)
+
 
 class Faction(models.Model):
-    faction_id = models.ForeignKey(FactionList, on_delete=models.CASCADE, to_field='faction_id')
+    faction_id = models.ForeignKey(
+        FactionList, on_delete=models.CASCADE, to_field='faction_id')
     respect = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
