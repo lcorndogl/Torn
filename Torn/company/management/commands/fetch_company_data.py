5e46cd9c81939d57c5e07ac641fcd777325bda0b
lcorndogl <mik.reds.forever@gmail.com>
2025-06-16 21:19:56 +0100

    Fix company update script

--- Torn/company/management/commands/fetch_company_data.py
+++ Torn/company/management/commands/fetch_company_data.py
@@ -1,15 +1,26 @@
 import requests
+import environ
 from django.core.management.base import BaseCommand
 from company.models import Company, Employee
 from datetime import datetime
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+API_KEY = env('API_KEY')
+
 
 class Command(BaseCommand):
     help = 'Fetch company data from the Torn API and insert it into the database'
 
     def handle(self, *args, **kwargs):
-        url = 'https://api.torn.com/company/104351?selections=profile,employees&key={API_KEY}&comment=FetchCompany'
+        url = f'https://api.torn.com/company/104351?selections=profile,employees&key={API_KEY}&comment=FetchCompany'
         response = requests.get(url)
         data = response.json()
+
+        print("Top-level keys:", data.keys())
+        print("Company keys:", data.get('company', {}).keys())
 
         # Check if the company data exists
         if 'company' in data:
@@ -33,17 +44,17 @@ class Command(BaseCommand):
                     company=company,
                     name=employee_data['name'],
                     position=employee_data['position'],
-                    manual_labour=employee_data['manual_labor'],
-                    intelligence=employee_data['intelligence'],
-                    endurance=employee_data['endurance'],
-                    effectiveness_working_stats=employee_data['effectiveness'].get('working_stats', 0),
-                    effectiveness_settled_in=employee_data['effectiveness'].get('settled_in', 0),
-                    effectiveness_merits=employee_data['effectiveness'].get('merits', 0),
-                    effectiveness_director_education=employee_data['effectiveness'].get('director_education', 0),
-                    effectiveness_management=employee_data['effectiveness'].get('management', 0),
-                    effectiveness_inactivity=employee_data['effectiveness'].get('inactivity', 0),
-                    effectiveness_addiction=employee_data['effectiveness'].get('addiction', 0),
-                    effectiveness_total=employee_data['effectiveness'].get('total', 0),
+                    manual_labour=employee_data.get('manual_labor', 0),
+                    intelligence=employee_data.get('intelligence', 0),
+                    endurance=employee_data.get('endurance', 0),
+                    effectiveness_working_stats=employee_data.get('effectiveness', {}).get('working_stats', 0),
+                    effectiveness_settled_in=employee_data.get('effectiveness', {}).get('settled_in', 0),
+                    effectiveness_merits=employee_data.get('effectiveness', {}).get('merits', 0),
+                    effectiveness_director_education=employee_data.get('effectiveness', {}).get('director_education', 0),
+                    effectiveness_management=employee_data.get('effectiveness', {}).get('management', 0),
+                    effectiveness_inactivity=employee_data.get('effectiveness', {}).get('inactivity', 0),
+                    effectiveness_addiction=employee_data.get('effectiveness', {}).get('addiction', 0),
+                    effectiveness_total=employee_data.get('effectiveness', {}).get('total', 0),
                     last_action_status=employee_data['last_action']['status'],
                     last_action_timestamp=datetime.fromtimestamp(employee_data['last_action']['timestamp']),
                     last_action_relative=employee_data['last_action']['relative'],
