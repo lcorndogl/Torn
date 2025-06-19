93e4469af53f63a514e283cf70f4ee01c34d0598
Michael Cornall <mik.reds.forever@gmail.com>
2025-06-19 14:56:24 +0100

    Add page for tracking company employee effectiveness over time

--- Torn/company/urls.py
+++ Torn/company/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('eg', views.eternal_workstats, name='eternal_workstats'),
     path('', views.company_list, name='company_list'),
 ]
--- Torn/company/views.py
+++ Torn/company/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from .models import Company
+from .models import Company, Employee
+import json
+from django.utils.safestring import mark_safe
 
 def company_list(request):
     companies = Company.objects.all()
-    return render(request, 'company_list.html', {'companies': companies})
+    return render(request, 'company/company_list.html', {'companies': companies})
+
+def eternal_workstats(request):
+    employees = Employee.objects.all()
+    # Prepare data for chart: include manual_labour, intelligence, endurance
+    employee_data = list(employees.values(
+        'employee_id', 'name', 'created_on', 'effectiveness_working_stats',
+        'manual_labour', 'intelligence', 'endurance'))
+    employee_data_json = mark_safe(json.dumps(employee_data, default=str))
+    return render(request, 'company/eternal_workstats.html', {
+        'employees': employees,
+        'employee_data_json': employee_data_json
+    })
