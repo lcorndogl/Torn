9d5ea03dccc2e51540ebc1f0470107975170265a
Michael Cornall <mik.reds.forever@gmail.com>
2025-11-10 18:38:30 +0000

    Fix employee data sorting by created_on date to ensure charts work correctly

--- Torn/company/views.py
+++ Torn/company/views.py
@@ -8,8 +8,8 @@ def company_list(request):
     return render(request, 'company/company_list.html', {'companies': companies})
 
 def eternal_workstats(request):
-    # Filter employees to only show those from company ID 110380
-    employees = Employee.objects.filter(company__company_id=104351)
+    # Filter employees to only show those from company ID 104351, exclude total effectiveness of 0, ordered by created_on
+    employees = Employee.objects.filter(company__company_id=104351, effectiveness_total__gt=0).order_by('created_on')
     # Prepare data for chart: include manual_labour, intelligence, endurance, and addiction
     employee_data = list(employees.values(
         'employee_id', 'name', 'created_on', 'effectiveness_working_stats',
@@ -21,8 +21,8 @@ def eternal_workstats(request):
     })
 
 def employees(request):
-    # Filter employees to only show those from company ID 110380
-    employees = Employee.objects.filter(company__company_id=110380)
+    # Filter employees to only show those from company ID 110380, exclude total effectiveness of 0, ordered by created_on
+    employees = Employee.objects.filter(company__company_id=110380, effectiveness_total__gt=0).order_by('created_on')
     
     # Get the most recent created_on date to identify current members
     from django.db.models import Max
